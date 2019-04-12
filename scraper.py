540ad5db0354c1850c688ad8adcda48712aea2d8
ThePhiri <chipoephiri@gmail.com>
2019-04-12 11:00:54 +0200

    web scrapping complete

--- scraper.py
+++ scraper.py
@@ -1,19 +1,40 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+import os
 
+#set website
 source = requests.get('https://www.property24.com/houses-to-rent/johannesburg/gauteng/100#SortOrder').text
-
 soup = BeautifulSoup(source, 'lxml')
 
+#csv to be written to
+hse_data = open('p24.csv', 'a')
+csv_writer = csv.writer(hse_data)
+
+exists = os.path.isfile('p24.csv')
+
+if exists != True:
+    csv_writer.writerow(['title', 'price', 'excerpt', 'area'])
+
+
+#extract content
 for all_content in soup.find_all('span', class_='p24_content'):
-    title = all_content.find('span', class_='p24_title')
-    price = all_content.find('span', class_='p24_price')
-    features = all_content.find('span', class_='p24_features')
-    excerpt = all_content.find('span', class_='p24_excerpt')
-    area = all_content.find('span', class_='p24_bold')
-    print(title.text)
-    print(price.text)
-    print(features.text)
-    print(excerpt.text)
-    print(area.text)
-    print('****************************')
+    try:
+        title = all_content.find('span', class_='p24_title').text
+        price = all_content.find('span', class_='p24_price').text
+        excerpt = all_content.find('span', class_='p24_excerpt').text
+        area = all_content.find('span', class_='p24_bold')
+
+        if area == None:
+            print("The area is no there.")
+        else:
+            area = area.text
+        print(title)
+        print(price)
+        print(excerpt)
+        print(area)
+        print('****************************')
+    except:
+        pass
+    csv_writer.writerow([title, price, excerpt, area])
+hse_data.close()
